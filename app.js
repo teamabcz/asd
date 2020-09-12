461a6d0301d6e10cca2e904be58cdc823c6a0cca
NISHANT KETU <nketu06@gmail.com>
2020-09-12 15:05:52 +0530

    added all directory

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const bodyParser=require("body-parser");
+
+
+const app =express();
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.use(express.static("public"));
+
+
+app.get("/",function(req,res){
+res.sendFile(__dirname+"/index.html")
+
+})
+
+
+
+
+
+
+app.listen(3000,function(){
+  console.log("server is running at 3000");
+})
---
+++ public/jscr/function.js
@@ -0,0 +1,5 @@
+document.getElementById("h2tag").addEventListener("click",displayDate)
+
+function displayDate() {
+  document.getElementById("h2tag").innerHTML = "finally working"
+}
