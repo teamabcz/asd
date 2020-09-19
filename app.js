1ffcb271a606522c9cc418e49c450191a83c43f8
NISHANT KETU <nketu06@gmail.com>
2020-09-19 18:13:47 +0530

    starting page

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser=require("body-parser");
 const app =express();
 app.use(bodyParser.urlencoded({extended:true}));
 
-app.use(express.static("public"));
+app.use(express.static("./public"));
 
 
 app.get("/",function(req,res){
--- public/jscr/function.js
+++ public/jscr/function.js
@@ -1,5 +1,43 @@
-document.getElementById("h2tag").addEventListener("click",displayDate)
 
-function displayDate() {
-  document.getElementById("h2tag").innerHTML = "finally working"
+
+
+function myFunction() {
+
+  var x = document.getElementById("myTopnav");
+  if (x.className === "navbar") {
+    x.className += " responsive";
+  } else {
+    x.className = "navbar";
+  }
+}
+
+function navfunction(){
+  var y = document.getElementById("myMainnav");
+  if (y.className === "mainnav m-row") {
+    y.className += " navresponsive";
+  console.log(y.className)
+  } else {
+    y.className = "mainnav m-row";
+  }
+
+}
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
